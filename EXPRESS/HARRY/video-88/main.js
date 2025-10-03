1c240e9c7fe723ba5bb6ba8ec931f3a3b4b3d444
Shahrukh <sa2672001@gmail.com>
2025-10-03 20:36:09 +0500

    Added new practice code

--- EXPRESS/HARRY/video-88/main.js
+++ EXPRESS/HARRY/video-88/main.js
@@ -2,19 +2,33 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-// For Public File
 
-app.use(express.static("public"));
+
 
 app.get("/", (req, res) => {
-  res.send("Hello World3!");
+  res.send("Hello World!");
 });
-app.get("/:slug", (req, res) => {
-  res.send(`Hello ${req.params.slug} `);
-});
-app.get("/:slug/:secondSlug", (req, res) => {
-  res.send(`Hello ${req.params.slug} and ${req.params.secondSlug}  `);
-});
+
+// app.get("/:slug", (req, res) => {
+//   res.send(`Hello ${req.params.slug} `);
+// });
+// app.get("/:slug/:secondSlug", (req, res) => {
+//   res.send(`Hello ${req.params.slug} and ${req.params.secondSlug}  `);
+// });
+
+// app.get("/:slug", (req, res) => {
+//   res.send(`Welcome to ${req.params.slug} Page`);
+//   console.log(req.params);
+//   console.log(req.query);
+// });
+
+
+// Serving static files in Express
+// To serve static files such as images, CSS files, and JavaScript files, use the express.static built-in middleware function in Express.
+
+// The function signature is:
+
+app.use(express.static("public"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
